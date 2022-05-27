12ba6d21a3030c10aa8ba46e6416e89cc96ce7cd
adamhinton <adam.d.hinton@gmail.com>
2022-05-26 22:12:49 -0400

    pseudo code plan

--- app.js
+++ app.js
@@ -28,3 +28,9 @@ const spinWords = (string) => {
 // Digital root is the recursive sum of all the digits in a number.
 
 // Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// PLAN:
+// take in n (int > 0)
+// Find sum of digits of n
+// If digits.length > 1:
+// continue reducing until single digit
