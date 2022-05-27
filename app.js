4950d0df2c07f89692d0629672d456e3b69ad0db
adamhinton <adam.d.hinton@gmail.com>
2022-05-26 22:01:26 -0400

    skeleton fxn

--- app.js
+++ app.js
@@ -3,3 +3,5 @@
 // Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 
 // Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+
+const spinWords = (string) => {};
