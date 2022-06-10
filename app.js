1dbb792e08ddbd5db07a0b333fb67354ee2e5f54
adamhinton <adam.d.hinton@gmail.com>
2022-06-10 14:30:19 -0400

    skeleton plan

--- app.js
+++ app.js
@@ -108,4 +108,9 @@
 
 // Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
 
+// PLAN:
+// let count = 0;
+
+// return count;
+
 const persistence = (num) => {};
