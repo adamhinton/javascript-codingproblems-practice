31deb78f7e926412878b0f479b1be6a98db97adb
adamhinton <adam.d.hinton@gmail.com>
2022-06-10 14:32:00 -0400

    plan for support fxn

--- app.js
+++ app.js
@@ -109,6 +109,8 @@
 // Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
 
 // PLAN:
+// first, write fxn that takes in num and returns all its digits multiplied together
+
 // let count = 0;
 // while num is > 9:
 // return persistence (all digits of num multiplied together)
